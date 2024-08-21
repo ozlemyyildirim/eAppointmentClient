48a2447162e88678f38be471704aa29e0e76f64a
ÖZLEM YILDIRIM FINDIK <ozlem.findik@saglik.gov.tr>
2024-08-21 11:20:42 +0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { inject } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { DoctorsComponent } from './components/doctors/doctors.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "",
+        component: LayoutsComponent,
+        canActivateChild: [()=> inject(AuthService).isAuthenticated()],
+        children: [
+            {
+                path: "",
+                component: HomeComponent
+            },
+            {
+                path: "doctors",
+                component: DoctorsComponent
+            }
+        ]
+    },
+    {
+        path: "**",
+        component: NotFoundComponent
+    }
+];
---
+++ src/app/constants.ts
@@ -0,0 +1,49 @@
+import { DepartmentModel } from "./models/doctor.model"
+
+export const api:string="https://localhost:7269/api"
+
+export const departments:DepartmentModel[]=[
+{
+    value:1,
+    name:"Acil"
+},
+{
+    value:2,
+    name:"Radyoloji"
+},
+{
+    value:3,
+    name:"Nöroloji"
+},
+{
+    value:4,
+    name:"Genel Cerrahi"
+},
+{
+    value:5,
+    name:"Endokrolonoji"
+},
+{
+    value:6,
+    name:"Hematoloji"
+},
+{
+    value:7,
+    name:"Ortopedi"
+},
+{
+    value:8,
+    name:"Psikiyatri"
+},
+{
+    value:9,
+    name:"Pediatri"
+},
+{
+    value:10,
+    name:"Kardiyoloji"
+}
+
+];
+
+
